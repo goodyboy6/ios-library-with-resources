daacf409da6e8da0f7b0a694eda8e65278056feb
yixiaoluo <xiaoluo.yxl@alibaba-inc.com>
2016-06-13 20:07:19 +0800

    add png referrence

--- MyLibrary/MyLibrary/MyViewController.m
+++ MyLibrary/MyLibrary/MyViewController.m
@@ -9,4 +9,15 @@
     return self;
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImage *img = [UIImage imageNamed:@"MyLibraryResources.bundle/img1"];
+    UIImageView *imgView = [[UIImageView alloc] initWithImage:img];
+    imgView.frame = self.view.bounds;
+    imgView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:imgView];
+}
+
 @end
--- MyLibraryTest/MyLibraryTest/AppDelegate.m
+++ MyLibraryTest/MyLibraryTest/AppDelegate.m
@@ -14,6 +14,13 @@
     self.window.rootViewController = viewController;
     
     [self.window makeKeyAndVisible];
+    
+    UIImage *img = [UIImage imageNamed:@"MyLibraryResources.bundle/img2"];
+    UIImageView *imgView = [[UIImageView alloc] initWithImage:img];
+    imgView.frame = self.window.bounds;
+    imgView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.window addSubview:imgView];
+    
     return YES;
 }
 
